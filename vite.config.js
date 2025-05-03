90b0cc301b77f9cc0b768f4db689b94caafa4e0f
BendibHafed <hafededdinebendib@gmail.com>
2025-05-03 12:39:56 +0200

    FrontEnd refractoring 1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/face-recognition-fullstack-1",
+  base: "/face-recognition-frontend",
   server: {
     proxy: {
       '/clarifai': {  // 👈 Requests starting with `/clarifai` will be proxied
